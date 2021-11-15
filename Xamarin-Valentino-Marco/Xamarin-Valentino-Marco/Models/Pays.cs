f45f2b98d87ab5ed881ade3cbde42f2454a068d8
val_mzn <valentinomanzon@gmail.com>
2021-11-15 11:02:01 +0100

    models

---
+++ Xamarin-Valentino-Marco/Xamarin-Valentino-Marco/Models/Pays.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin_Valentino_Marco.Models
+{
+    public class Pays
+    {
+        public string Nom { get; set; }
+    }
+}
---
+++ Xamarin-Valentino-Marco/Xamarin-Valentino-Marco/Models/Ville.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin_Valentino_Marco.Models
+{
+    public class Ville
+    {
+        public string Nom { get; set; }
+        public string Commentaire { get; set; }
+        public int Cp { get; set; }
+        public Pays Pays { get; set; }
+    }
+}
